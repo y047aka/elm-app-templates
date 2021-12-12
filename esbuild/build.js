19079180715a99e551b3056d625ca93781744b6c
Yoshitaka Totsuka <y047aka@gmail.com>
2021-12-12 21:21:31 +0900

    Add esbuild

---
+++ esbuild/build.js
@@ -0,0 +1,15 @@
+const esbuild = require('esbuild');
+const ElmPlugin = require('esbuild-plugin-elm');
+
+esbuild.build({
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  outdir: 'dist',
+  watch: process.argv.includes('--watch'),
+  plugins: [
+    ElmPlugin({
+      debug: true,
+      clearOnWatch: true,
+    }),
+  ],
+}).catch(_e => process.exit(1))
