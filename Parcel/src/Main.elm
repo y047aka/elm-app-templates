923dbf0f5f3998b495ec3a283900ba28aa1fe0eb
Yoshitaka Totsuka <y047aka@gmail.com>
2021-12-12 12:01:51 +0900

    Add Parcel

---
+++ Parcel/src/Main.elm
@@ -0,0 +1,57 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Html exposing (main_, text)
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view _ =
+    { title = ""
+    , body =
+        [ main_ [] [ text "elm-supercell" ]
+        ]
+    }
