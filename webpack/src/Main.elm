51a4975953953b2203320b84c563139d7bc6f24c
Yoshitaka Totsuka <y047aka@gmail.com>
2021-12-30 00:13:00 +0900

    Fix styles

--- webpack/src/Main.elm
+++ webpack/src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
@@ -53,7 +54,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
+    div [ class "counter" ]
         [ button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment ] [ text "+" ]
